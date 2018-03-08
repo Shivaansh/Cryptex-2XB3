f7096645ba09281f0d84de330b4affe3c9663a24
Somar Aani <somarani.1998@gmail.com>
2018-03-08 12:14:58 -0500

    Created sorting class

---
+++ src/util/sort/QuickSort.java
@@ -0,0 +1,18 @@
+package util.sort;
+
+import java.util.Comparator;
+import coin.Coin;
+
+public class QuickSort {
+	
+	/**
+	 * Will sort the given coin list using the Comparator c
+	 * @param list coin list to be sorted
+	 * @param c comparator object to use to compare coins
+	 */
+	public static void sort(Coin[] list, Comparator<Coin> c) {
+		
+	}
+	
+	//add private helped / sorting methods here
+}

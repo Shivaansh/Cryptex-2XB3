e237184510cba7859d042ed17d992a996ed572c8
Somar Aani <somarani.1998@gmail.com>
2018-03-09 13:56:06 -0500

    Finished getDailyHistorical()

--- src/coin/Coin.java
+++ src/coin/Coin.java
@@ -1,5 +1,13 @@
 package coin;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 import util.APIHandler;
@@ -105,6 +113,53 @@ public class Coin{
 		return dailyChangePercent;
 	}
 	
+	/**
+	 * Gets the coins daily historical data in the date range specified. Dates must be less than 2000 days apart
+	 * @param relCoinCode base currency to use for coin value 
+	 * @param fromDate date from, format is "dd/MM/YYYY"
+	 * @param toDate date to, format is "dd/MM/YYY"
+	 * @return array including all coin prices from fromDate to toDate
+	 * @throws APINotRespondingException if API does not responds or responds with an error
+	 */
+	public double[] getDailyHistorical(String relCoinCode, String fromDate, String toDate) throws APINotRespondingException {
+	    DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+	   
+	    Date from;
+	    Date to;
+	    
+		try {
+			from = dateFormat.parse(fromDate);
+			to = dateFormat.parse(toDate);
+			
+		} catch (ParseException e) {
+			e.printStackTrace();
+			return null;
+		}
+	
+	    long toTs = to.getTime()/1000;
+		long diff = to.getTime() - from.getTime();
+	    long limit = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+	    
+	    System.out.println(limit);
+	    
+	    if(limit <= 0)
+	    	throw new IllegalArgumentException("DateTo must come after DateFrom");
+	    	
+	    if(limit > 2000)
+	    	throw new IllegalArgumentException("Dates cannot be more than 2000 dates apart");
+	    
+
+		JsonArray dataArray = APIHandler.request(CallType.HISTO_DAY, "fsym=" + this.code, "tsym=" + relCoinCode, "toTs=" +toTs, "limit=" + limit).getAsJsonArray("Data");
+		
+		double[] data = new double[dataArray.size()];
+		int i = 0;
+		for(JsonElement curr : dataArray) {
+			data[i++] = curr.getAsJsonObject().getAsJsonPrimitive("close").getAsDouble();
+		}
+		
+		return data;
+	}
+
 	@Override
 	public String toString() {
 		return name + " (" + code + ")";

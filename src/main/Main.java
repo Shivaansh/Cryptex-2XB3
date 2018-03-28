029c48967a9130031f1d71d6fa901a4e10e29e53
Shivaansh Prasann <shivaanshseal@gmail.com>
2018-03-28 00:49:48 -0400

    "Need  to implement public sorting method"

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,9 +8,14 @@ import util.Logger;
 public class Main {
 	public static void main(String args[]) {
 		try {
+            int count = 1;
 			CoinList.init();
 			CoinList.loadMarketData("USD");
-						
+			for(Coin c : CoinList.getList()){
+			    System.out.println(count + " " + c.getName() + " " + c.getPrice() + " " + c.getMarketCap() + " " + c.getDailyChangePercent());
+			    count ++;
+            }
+
 		} catch (APINotRespondingException e) {
 			Logger.error("API Not responding");
 			e.printStackTrace();
--- src/util/sort/QuickSort.java
+++ src/util/sort/QuickSort.java
@@ -1,9 +1,9 @@
 package util.sort;
 
 import java.util.Comparator;
-import coin.Coin;
+import java.lang.Comparable; //imported by Shivaansh
 
-public class QuickSort {
+public class quickSort {
 	
 	/**
 	 * Will sort the given coin list using the Comparator c
@@ -14,5 +14,55 @@ public class QuickSort {
 		
 	}
 	
-	//add private helped / sorting methods here
+	//add private helper / sorting methods here
+	//Reference: https://algs4.cs.princeton.edu/code/edu/princeton/cs/algs4/Quick.java.html
+
+	/**
+	 * Used to check if array is sorted
+	 * @param list coin list to be sorted
+	 */
+	private static boolean isSorted(Coin[] list) {
+        return isSorted(a, 0, a.length - 1);
+    }
+	
+	
+	
+	/**
+	 * Used to check if array is sorted
+	 * @param list coin list to be sorted
+	 * @param lo lower limit for sorting
+	 * @param hi upper limit for sorting
+	 */
+	private static boolean isSorted(Coin[] list, int lo, int hi) {
+        for (int i = lo + 1; i <= hi; i++)
+            if (less(a[i], a[i-1])) return false;
+        return true;
+    }
+	
+	
+	
+	/**
+	 * Used to check if one value is less than another
+	 * @param a the first value
+	 * @param b the second value
+	 */
+	private static boolean less(Comparable a, Comparable b) {
+        if (a == b) return false;   
+        return a.compareTo(b) < 0;
+    }
+        
+	
+	
+	/**
+	 * Used to exchange two values in an array
+	 * @param ob the array in which the swap takes place
+	 * @param i the first value
+	 * @param j the second value
+	 */
+    private static void swap(Coin[] ob, int i, int j) {
+        Coin exch = ob[i];
+        ob[i] = ob[j];
+        ob[j] = exch;
+    }
+	
 }

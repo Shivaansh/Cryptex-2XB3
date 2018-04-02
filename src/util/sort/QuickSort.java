874310c48adecc93e2e98ebac08e2e8db79e75a2
Shivaansh Prasann <shivaanshseal@gmail.com>
2018-04-02 14:01:38 -0400

    QuickSOrt revised"

--- src/util/sort/QuickSort.java
+++ src/util/sort/QuickSort.java
@@ -4,9 +4,7 @@ import java.util.Comparator;
 
 import coin.Coin;
 
-import java.lang.Comparable; //imported by Shivaansh
-
-public class quickSort {
+public class QuickSort implements Comparator<Coin>{
 	
 	/**
 	 * Will sort the given coin list using the Comparator c
@@ -15,7 +13,6 @@ public class quickSort {
 	 */
 	public static void sort(Coin[] list, Comparator<Coin> c) {
 		sort(list, 0, list.length-1);
-		assert(isSorted(list));
 	}
 	
 	//add private helper / sorting methods here
@@ -23,62 +20,57 @@ public class quickSort {
 
 	/**
 	 * Private sorting method to sort an array (works recursively)
-	 * @param a The array to be sorted
+	 * @param list The array to be sorted
 	 * @param lo The lower index for sorting
 	 * @param hi The upper index for sorting
 	 */
-    private static void sort(Comparable[] a, int lo, int hi) { 
+    private static void sort(Coin[] list, int lo, int hi) { 
         if (hi <= lo) return;
-        int j = partition(a, lo, hi);
-        sort(a, lo, j-1);
-        sort(a, j+1, hi);
-        assert isSorted(a, lo, hi);
+        int j = partition(list, lo, hi);
+        sort(list, lo, j-1);
+        sort(list, j+1, hi);
     }
-	/**
-	 * Used to check if array is sorted
-	 * @param list coin list to be sorted
-	 */
-	private static boolean isSorted(Coin[] list) {
-        return isSorted(list, 0, list.length - 1);
+
+
+    private static int partition(Coin[] a, int lo, int hi) {
+        int i = lo;
+        int j = hi + 1;
+       int v = (int) a[lo].getPrice();
+        while (true) { 
+
+            // find item on lo to swap
+            while (less1(a[++i], a[v])) {
+                if (i == hi) break;
+            }
+
+            // find item on hi to swap
+            while (less1(a[v], a[--j])) {
+                if (j == lo) break;      // redundant since a[lo] acts as sentinel
+            }
+
+            // check if pointers cross
+            if (i >= j) break;
+
+           swap(a, i, j);
+        }
+
+        // put partitioning item v at a[j]
+        swap(a, lo, j);
+
+        // now, a[lo .. j-1] <= a[j] <= a[j+1 .. hi]
+        return j;
     }
-	
-	
-	
-	/**
-	 * Used to check if array is sorted
-	 * @param list coin list to be sorted
-	 * @param lo lower limit for sorting
-	 * @param hi upper limit for sorting
-	 */
-	private static boolean isSorted(Coin[] list, int lo, int hi) {
-        for (int i = lo + 1; i <= hi; i++)
-            if (less(list[i], list[i-1])) return false;
-        return true;
-    }
-	
-	/**
-	 * Checks result of isSorted() and returns as boolean
-	 * @param a The array to be sorted
-	 * @return Boolean value: whether the array has been sorted
-	 */
-	private static boolean isSorted(Comparable[] a) {
-        return isSorted(a, 0, a.length - 1);
-    }
-	
-	
-	
+
 	/**
 	 * Used to check if one value is less than another
 	 * @param a the first value
 	 * @param b the second value
 	 */
-	private static boolean less(Comparable a, Comparable b, Comparator<Coin> c) {
-        if (a == b) return false;   
-        return (c.compare(a,b) < 0);
+	private static boolean less1(Coin a, Coin b) {
+        if (a.getPrice() == b.getPrice()) return false;   
+        return ((int) (a.getPrice() - b.getPrice()) < 0);
     }
-        
-	
-	
+        	
 	/**
 	 * Used to exchange two values in an array
 	 * @param ob the array in which the swap takes place
@@ -90,5 +82,11 @@ public class quickSort {
         ob[i] = ob[j];
         ob[j] = exch;
     }
+
+	@Override
+	public int compare(Coin o1, Coin o2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 	
 }

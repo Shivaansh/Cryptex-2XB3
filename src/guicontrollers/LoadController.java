191f931007b27f4d1f2ee59abdeaa9eb448e4bd4
Tasin <Tasin@192.168.1.3>
2018-04-02 13:58:38 -0400

    Page

--- src/guicontrollers/LoadController.java
+++ src/guicontrollers/LoadController.java
@@ -83,8 +83,8 @@ public class LoadController implements Initializable {
         window2.setTitle("Cryptex");
         window2.setScene(main);
         window2.show();
-        TableView<Coin> table = CoinMainController.getTable2();
-        setScroll(main, table);
+        //TableView<Coin> table = CoinMainController.getTable2();
+        //setScroll(main, table);
     }
 
     /**
@@ -95,11 +95,6 @@ public class LoadController implements Initializable {
         return new Task<Object>() {
             @Override
             protected Object call() throws Exception {
-                /*for(int i=count; i<100+count; i++) {
-                    int odd = (int) Math.pow(-1, i);
-                    coin.add(new Coin(i + 1, new ImageView(new Image("https://www.cryptocompare.com/media/20627/nxt.png", 50, 0, true, false)), "Bitcoin" + i, "$" + (i + 1) * 100, i * 10, odd * (i * 20) + "%"));
-                    updateProgress(i+1, 100);
-                }*/
                 Platform.runLater(new Runnable() {
                     @Override public void run() {
                         loadInfo.setText("Attempting to fetch data...");
@@ -112,7 +107,7 @@ public class LoadController implements Initializable {
                 }
                 
                 Coin[] list = CoinList.getList();
-                for(int i = count; i < count+100; i++){
+                for(int i = count; i < count+CoinList.MAX_MARKET_INPUT; i++){
                     if(i == CoinList.getList().length){
                         count = i;
                         bottom = true;
@@ -121,7 +116,7 @@ public class LoadController implements Initializable {
                     Coin c = list[i];
                     coinArray.add(c);
                     //System.out.println(CoinList.getList()[i].getDailyChangePercent());
-                    updateProgress(i+1, 100);
+                    updateProgress(i+1, CoinList.MAX_MARKET_INPUT);
                     Platform.runLater(new Runnable() {
                         @Override public void run() {
                             loadInfo.setText("Added " + c.getName());
@@ -133,20 +128,7 @@ public class LoadController implements Initializable {
         };
     }
 
-    private ScrollBar getTableViewScrollBar(TableView<?> table) {
-        ScrollBar scrollbar = null;
-        for (Node node : table.lookupAll(".scroll-bar")) {
-            if (node instanceof ScrollBar) {
-                ScrollBar bar = (ScrollBar) node;
-                if (bar.getOrientation().equals(Orientation.VERTICAL)) {
-                    scrollbar = bar;
-                }
-            }
-        }
-        return scrollbar;
-    }
-
-    public void setScroll(Scene main, TableView<Coin> table){
+    /*public void setScroll(Scene main, TableView<Coin> table){
         ScrollBar tScroll = getTableViewScrollBar(table);
         tScroll.valueProperty().addListener((observable, oldValue, newValue) -> {
             double position = newValue.doubleValue();
@@ -154,8 +136,7 @@ public class LoadController implements Initializable {
             if (position == scrollBar.getMax()) {
                 task = taskCreate();
                 if(!bottom){
-                    count+=100;
-                    main.setCursor(Cursor.WAIT);
+                    count+=CoinList.MAX_MARKET_INPUT;
                 }
                 task.addEventHandler(WorkerStateEvent.WORKER_STATE_SUCCEEDED,
                         new EventHandler<WorkerStateEvent>() {
@@ -168,7 +149,7 @@ public class LoadController implements Initializable {
                 CoinMainController.setCoinArray(coinArray);
             }
         });
-    }
+    }*/
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
